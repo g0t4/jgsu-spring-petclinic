7554f271b1283bf05e6e75a497e4e44e1f969895
Wes Higbee <wes.mcclure@gmail.com>
2020-09-08 05:02:24 -0400

    actually bork tests

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,8 +45,8 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("");
-		person.setLastName("smithy");
+		person.setFirstName("notempty");
+		person.setLastName("smith");
 
 		Validator validator = createValidator();
 		Set<ConstraintViolation<Person>> constraintViolations = validator.validate(person);
