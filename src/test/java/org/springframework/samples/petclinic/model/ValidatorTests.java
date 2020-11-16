adb4bca7f8fd4c006c9ec18f1a55a9930394fd49
vidyasailaja <74178949+vidyasailaja@users.noreply.github.com>
2020-11-16 14:17:44 +0100

    Update ValidatorTests.java
    changing test case

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("");
+		person.setFirstName("not empty");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
