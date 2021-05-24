3761f87888cc24d086c27840a9e9407d083fe5d9
Peter Ngoh <peter.ngoh@geutebrueck.com>
2021-05-24 17:10:50 +0200

    Broke test

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("");
+		person.setFirstName("notEmpty");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
