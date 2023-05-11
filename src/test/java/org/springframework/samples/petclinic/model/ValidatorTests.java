25c88d7086feea9070979241fce18540f4574bbf
trishahota <trisha.hota@gmail.com>
2023-05-12 01:59:06 +0400

    breaking test

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("");
+		person.setFirstName("NotEmpty");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
